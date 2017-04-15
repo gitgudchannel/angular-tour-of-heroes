89640fdafee6693b51ace0d58bc6d3e557ab0ab8
Josip Ledic <josip.ledic@outlook.com>
2017-04-15 19:46:20 +0200

    tour of heroes intellij git commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,42 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }         from './app.component';
+import { HeroDetailComponent }  from "./hero-detail.component";
+import { HeroService }          from "./hero.service";
+import { HeroesComponent }      from "./heroes.component";
+import { DashboardComponent }   from "./dashboard.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:[
+    BrowserModule,
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+  ],
+  declarations:[
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  bootstrap:[
+    AppComponent
+  ],
+  providers:[
+    HeroService
+  ]
 })
-export class AppModule { }
+
+
+export class AppModule {
+
+}
---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Josip on 13.04.2017.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
